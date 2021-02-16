9c8434ec160016d562b842026009d45503a3b90e
Mateusz Krupa <mateuszandrzejkrupa@gmail.com>
2021-02-16 15:01:33 +0100

    Add container component

---
+++ src/components/Container/Container.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './Container.scss';
+import PropTypes from 'prop-types';
+
+
+class Container extends React.Component {
+    static propTypes = {
+      children: PropTypes.node,
+    }
+    
+    static defaultProps = { 
+      children: <p>children</p>,
+    }
+
+    render() {
+      const {children} = this.props;
+      return (
+        <section className={styles.component}>
+          <div className={styles.component}>{children}</div>
+        </section>
+      ); 
+    }
+}
+
+export default Container; 
